928a368d4334a5cc1247f05aeac77dd8e7568963
EvanEnev <evanenev@outlook.com>
2024-02-06 15:57:30 +0300

    :recycle: Update path to logo

--- lab1/index.php
+++ lab1/index.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles.css ">
     <title>2.1</title>
 </head>
 <body>
     <header class="header">
-        <img src="/public/images/polytech_logo.png" alt="polytech_logo">
+        <img src="./public/images/polytech_logo.png" alt="polytech_logo" width="100" height="100">
         <h1>2.1. Hello, World!</h1>
     </header>
 
